5ebf337a5e6df7ed2381e496ea92b169d8878606
0xkarl <karl.schwtz@gmail.com>
2022-03-16 04:40:22 +0300

    feat: use hash router

--- src/components/global/Layout.tsx
+++ src/components/global/Layout.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Header from './Header';
 
